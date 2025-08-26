12ddbf6b4e47e72dc18daf3f5241443874777485
Mokshith Reddy <mokshithreddy1802@gmail.com>
2025-08-26 21:58:35 +0530

    Updated backend server URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
-let IS_PROD= false;
+let IS_PROD= true;
 const server=IS_PROD ?
-    "https://videoconfirence.onrender.com":
+    "https://video-confirence.onrender.com":
 
     "http://localhost:8000"
     

6b213243b04f059711a9eb76068b6c329232a709
Mokshith Reddy <mokshithreddy1802@gmail.com>
2025-08-26 22:25:14 +0530

    Updated backend server URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ connectToSocket(server);
 app.set("port", (process.env.PORT || 8000));
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://videoconferencefrontend-lhwwx.onrender.com",
     methods: ["GET", "POST"]
 }));
 app.use(express.json({limit: "40kb"}));
